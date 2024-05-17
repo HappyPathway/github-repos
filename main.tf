1f0f18d3498ae20dc5f1ae859a05c0a957f11b78
Dave Arnold <dave@roknsound.com>
2024-05-17 09:24:39 -0700

    adding chromebook-venv repo

--- main.tf
+++ main.tf
@@ -5,3 +5,12 @@ module "repo" {
   repo_org = "HappyPathway"
   name = "aws-codebuild-image-pipeline"
 }
+
+module "chromebook_venv" {
+  source  = "app.terraform.io/roknsound/repo/github"
+  force_name = true
+  github_is_private = false
+  repo_org = "HappyPathway"
+  name = "chromebook-venv"
+  is_private = true
+}
