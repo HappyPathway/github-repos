009a9c39bcf9cb2d914d792717dcca5ed025adc1
David Arnold <10138997+djaboxx@users.noreply.github.com>
2024-08-21 14:28:59 -0700

    centralized-actions

--- main.tf
+++ main.tf
@@ -36,3 +36,13 @@ module "seed-workspace" {
   enforce_prs       = false
 }
 
+
+module "centralized-actions" {
+  source            = "app.terraform.io/roknsound/repo/github"
+  force_name        = true
+  github_is_private = false
+  repo_org          = "HappyPathway"
+  name              = "centralized-actions"
+  enforce_prs       = true
+}
+
