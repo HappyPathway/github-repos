5335cfbb1b67317d684c72a4a834edb990a5f0bc
Dave Arnold <dave@roknsound.com>
2024-08-21 22:05:12 -0700

    refactor: Update env.tf to use Google Secret Manager for GitHub token

--- env.tf
+++ env.tf
@@ -1,10 +1,10 @@
-# module "var" {
-#   source  = "HappyPathway/var/env"
-#   env_var = "GITHUB_TOKEN"
-# }
+data "google_secret_manager_secret_version" "basic" {
+  project = "happypathway-1522441039906"
+  secret  = "github_token"
+}
 
-# resource "github_actions_organization_secret" "example_secret" {
-#   secret_name     = "GH_TOKEN"
-#   visibility      = "all"
-#   plaintext_value = module.var.value
-# }
+resource "github_actions_organization_secret" "example_secret" {
+  secret_name     = "GH_TOKEN"
+  visibility      = "all"
+  plaintext_value = data.google_secret_manager_secret_version.basic.secret_data
+}
