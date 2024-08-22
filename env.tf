33c6a70cce520f4f657d55ec866fd37be3485071
Dave Arnold <dave@roknsound.com>
2024-08-21 21:53:26 -0700

    fxing

--- env.tf
+++ env.tf
@@ -1,10 +1,10 @@
-module "var" {
-  source  = "HappyPathway/var/env"
-  env_var = "GITHUB_TOKEN"
-}
+# module "var" {
+#   source  = "HappyPathway/var/env"
+#   env_var = "GITHUB_TOKEN"
+# }
 
-resource "github_actions_organization_secret" "example_secret" {
-  secret_name     = "GH_TOKEN"
-  visibility      = "all"
-  plaintext_value = module.var.value
-}
+# resource "github_actions_organization_secret" "example_secret" {
+#   secret_name     = "GH_TOKEN"
+#   visibility      = "all"
+#   plaintext_value = module.var.value
+# }
