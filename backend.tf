36a47a97dcf097c1a16f0ac52f594b304dcc279a
Dave Arnold <dave@roknsound.com>
2024-09-11 22:16:16 -0700

    Update .gitignore and remove .terraform.lock.hcl

--- backend.tf
+++ backend.tf
@@ -1,12 +1,3 @@
-# terraform {
-#   backend "remote" {
-#     organization = "roknsound"
-#     workspaces {
-#       prefix = "github-repos-"
-#     }
-#   }
-# }
-
 terraform {
   backend "gcs" {}
 }
